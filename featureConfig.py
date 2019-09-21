a18e0f9593d2f3db0437d83986f288a2b5f601b0
Daniel Wasserman <wassermandaniel8@gmail.com>
2019-09-21 17:16:59 +0000

    Update featureConfig

--- featureConfig.py
+++ featureConfig.py
@@ -3,8 +3,8 @@
 day = 86400
 
 # Periods in seconds
-default_periods = [60, 300, 900, 1800, 3600, 3600*4, int(day/2), day]
-raw_freqs = ['60s']
+default_periods = [300, 3600, 3600*4, day]
+raw_freqs = ['300s']
 raw_periods = {
     'volume': default_periods,
     'vwap': default_periods,
@@ -19,13 +19,13 @@ raw_periods = {
     'max_change': default_periods,
 }
 
-moving_avg_freqs = ['60s', '3600s', '21600s', '86400s']
+moving_avg_freqs = ['300s', '3600s', '21600s', '86400s']
 moving_avg_periods = {
     'sma': [5, 10, 20, 50, 100],
     'ema': [5, 10, 20, 50, 100]
 }
 
-indicator_freqs = ['60s', '3600s', '21600s', '86400s']
+indicator_freqs = ['300s', '3600s', '21600s', '86400s']
 
 s_r_freqs = ['6h']
 s_r_periods = [120]
